d92378ab5d7017a05590b965e7210e678b438e58
Kottofey <romeokontakt@yandex.ru>
2026-01-05 23:30:18 +0300

    [add] проверка на существование папки для скачивания

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,19 @@ import chalk from 'chalk';
 import { type FileFlavor, hydrateFiles } from '@grammyjs/files';
 
 import * as process from 'node:process';
+import { access } from 'node:fs/promises';
 
 type MyContext = FileFlavor<Context>;
+
+import * as path from 'node:path';
+
+import * as constants from 'node:constants';
 
 import { checkId } from './cmds';
 
 dotenv.config({ path: 'src/.env', quiet: true });
 
-const { TG_BOT_TOKEN, SAVE_DIR } = process.env;
+const { TG_BOT_TOKEN, SAVE_DIR, ADMIN_TG_ID } = process.env;
 
 try {
   if (!TG_BOT_TOKEN) {
@@ -20,6 +25,14 @@ try {
   }
   const bot = new Bot<MyContext>(TG_BOT_TOKEN);
   bot.api.config.use(hydrateFiles(bot.token));
+
+  // проверяем существование папки для скачивания
+  try {
+    if (SAVE_DIR) await access(SAVE_DIR, constants.F_OK);
+  } catch (err) {
+    if (ADMIN_TG_ID) await bot.api.sendMessage(ADMIN_TG_ID, `Папки для скачивания не существует, проверьте файл .env`);
+    console.error(chalk.red('Папки для скачивания не существует, проверьте файл .env\n\n'), err);
+  }
 
   bot.on('message:document', async (ctx) => {
     const senderId = ctx.message.from.id;
@@ -40,7 +53,9 @@ try {
     const file_id = ctx.message.document.file_id;
     const file_name = ctx.message.document.file_name;
     const file = await ctx.api.getFile(file_id);
+
     await file.download(`${SAVE_DIR}/${file_name}`);
+
     await ctx.api.sendMessage(
       senderId,
       `Готово.\n\nФайл: ${file_name}\n\nРазмер: ${file.file_size ? (file.file_size / 1024).toFixed(2) : '?'} КиБ`,
